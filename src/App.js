e3b249e6992bab895ca9523ba4ad4c3a1a2193f8
franklinnnn <franklin.assa@gmail.com>
2022-09-02 14:49:59 -0700

    error fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -36,85 +35,6 @@ function App() {
         </main>
       </BrowserRouter>
     </div>
-=======
-import React, { useState } from "react";
-import { Switch, Route, Redirect, NavLink } from "react-router-dom";
-
-import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
-import { ContactsPage } from "./containers/contactsPage/ContactsPage";
-import { TodosPage } from "./containers/todosPage/TodosPage";
-
-function App() {
-  const [appointments, setAppointments] = useState([]);
-  const [contacts, setContacts] = useState([]);
-
-  const ROUTES = {
-    CONTACTS: "/contacts",
-    APPOINTMENTS: "/appointments",
-    TODOS: "/todos"
-  };
-
-  const addAppointment = (title, contact, date, time) => {
-    setAppointments([
-      ...appointments,
-      {
-        title: title,
-        contact: contact,
-        date: date,
-        time: time,
-      },
-    ]);
-  };
-
-  const addContact = (name, phone, email) => {
-    setContacts([
-      ...contacts,
-      {
-        name: name,
-        phone: phone,
-        email: email,
-      },
-    ]);
-  };
-
-  return (
-    <>
-      <nav>
-        <div class="title-container">
-          <h1>Appointment Planner</h1>
-        </div>
-        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
-          Contacts
-        </NavLink>
-        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
-          Appointments
-        </NavLink>
-        <NavLink to={ROUTES.TODOS} activeClassName="active">
-          To Do's
-        </NavLink>
-      </nav>
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to={ROUTES.CONTACTS} />
-          </Route>
-          <Route path={ROUTES.CONTACTS}>
-            <ContactsPage contacts={contacts} addContact={addContact} />
-          </Route>
-          <Route path={ROUTES.APPOINTMENTS}>
-            <AppointmentsPage
-              appointments={appointments}
-              addAppointment={addAppointment}
-              contacts={contacts}
-            />
-          </Route>
-          <Route path={ROUTES.TODOS}>
-            <TodosPage />
-          </Route>
-        </Switch>
-      </main>
-    </>
->>>>>>> b8ad17b2a7623a889462115600b14a4f1826f69b
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -14,23 +13,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import store from './redux/store';
-import { Provider } from 'react-redux';
-
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
->>>>>>> b8ad17b2a7623a889462115600b14a4f1826f69b
